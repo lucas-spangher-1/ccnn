a326c2b675505c29c4b81c16761ba93ce8e9fb8d
spangher <spangher@node2319.inband>
2023-08-16 22:31:17 -0400

    this branch is just meant to find out what may be causing the data dim issue

--- sbatch_submit.sh
+++ sbatch_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ccnn_sweep
+#SBATCH --job-name=lucas_ccnn
 #SBATCH -N 1
 #SBATCH --gres=gpu
 #SBATCH --time=7:00:00
@@ -13,14 +13,13 @@ module load anaconda3/2022.10
 source /home/software/anaconda3/2022.10/etc/profile.d/conda.sh
 conda activate will-env
 
-
-cd /home/spangher/ccnn
 . ./venv/bin/activate
 
-# Ignore the bogus local user packages on the host
 export PYTHONUSERBASE=intentionally-disabled
 # Run multiple instantiations of the WandB agent in parallel
-export WANDB_API_KEY=d38dd9743cf22de213611707a16c0ef5beb0ee83
+export WANDB_API_KEY=19a95b1c7d3cc50ffeed9b952b0b7477bdcc0bd6
+export WANDB_DIR=/dev/shm/ccnn
+export WANDB_CACHE_DIR=/dev/shm/ccnn-cache
 wandb agent "$1"
 
 # Deactivate your virtual environment
